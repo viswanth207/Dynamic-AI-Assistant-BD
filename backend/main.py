b2bc48bea44b69c57bce1e7306fad6b937eee605
viswanth <viswanthchirumamilla@gmail.com>
2026-01-24 14:27:12 +0530

    Allow all CORS origins for deployment

--- backend/main.py
+++ backend/main.py
@@ -56,7 +56,7 @@ async def shutdown_db_client():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:3001", "http://127.0.0.1:3001", "http://localhost:8000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
